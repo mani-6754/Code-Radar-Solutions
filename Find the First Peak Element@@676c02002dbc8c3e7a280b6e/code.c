f3bf22d850d2efa1eabbcf1f6ab7b28839bcee14
Manureet Kaur Saluja <msaluja6754@gmail.com>
2025-03-18 09:48:09 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,23 +1,45 @@
+// #include <stdio.h>
+
+// int peakElement(int arr[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         int left = 1;
+//         int right = 1;
+//         if (i > 0 && arr[i] <= arr[i - 1])
+//             left = 0;
+//         if (i < n - 1 && arr[i] <= arr[i + 1])
+//             right = 0;
+//         if (left && right) {
+//             return i;
+//         }
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int arr[] = {1, 2, 4, 5, 7, 8, 3};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     printf("%d\n", peakElement(arr, n));
+//     return 0;
+// }
+
 #include <stdio.h>
-
-int peakElement(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        int left = 1;
-        int right = 1;
-        if (i > 0 && arr[i] <= arr[i - 1])
-            left = 0;
-        if (i < n - 1 && arr[i] <= arr[i + 1])
-            right = 0;
-        if (left && right) {
-            return i;
-        }
+int main()
+{
+    int n,a;
+    scanf("%d\n",&n);
+    int s[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a);
+        s[i]=a;
     }
-    return 0;
+    for(int j=0;j<n-1;j++)
+    {
+        if(s[j+1]>s[j])
+        {
+            printf("%d",s[j+1]);
+            break;
+        }
+            }
 }
-
-int main() {
-    int arr[] = {1, 2, 4, 5, 7, 8, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("%d\n", peakElement(arr, n));
-    return 0;
-}
+    
