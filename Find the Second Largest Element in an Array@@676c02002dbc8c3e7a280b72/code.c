9798908bb23e03f750029e678845a70ac8e989c1
Manureet Kaur Saluja <msaluja6754@gmail.com>
2025-03-31 00:01:41 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,28 +1,55 @@
-#include<stdio.h>
-int main()
-{
-    int n,a;
-    scanf("%d",&n);
+#include <stdio.h>
+
+int main() {
+    int n, a;
+
+    // Input size of array
+    scanf("%d", &n);
+
     int s[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&a);
-        s[i]=a;
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        s[i] = a;
     }
-    int max=s[0],max2=-1;
-    for(int j=0;j<n;j++)
-    {
-        if(max<s[j])
-        {
-            max=s[j];
+
+    // Initialize max and max2
+    int max = s[0], max2 = s[0];
+    int allSame = 1;  // Flag to check if all elements are the same
+
+    // Find the maximum element and check if all elements are the same
+    for (int j = 1; j < n; j++) {
+        if (s[j] != s[0]) {
+            allSame = 0;  // Found a different element
+        }
+        if (s[j] > max) {
+            max = s[j];
         }
     }
-    for(int j=0;j<n;j++)
-    {
-         if(max2<s[j]&&s[j]!=max)
-        {
-            max2=s[j];
+
+    // If all elements are the same, print -1
+    if (allSame) {
+        printf("-1\n");
+        return 0;
+    }
+
+    // Initialize max2 to a valid value smaller than max
+    max2 = (s[0] == max) ? s[1] : s[0];
+
+    // Find the second maximum element
+    for (int j = 0; j < n; j++) {
+        if (s[j] > max2 && s[j] != max) {
+            max2 = s[j];
         }
     }
-    printf("%d",max2);
+
+    // Check if max2 is still equal to max, meaning no valid second max
+    if (max2 == max) {
+        printf("-1\n");
+    } else {
+        printf("%d\n", max2);  // Print second largest element
+    }
+
+    return 0;
 }
